6b088fbb528f7f228c9f0b57789e868e77648fea
Evgeny Bubnov <Vitayaka87@gmail.com>
2020-08-05 14:04:19 +0300

    abram

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
+    std::vector< std::vector<int> > A(5, std::vector<int>(2));
 
-    return 0;
+    printf("str = %d\n", A.size());
+    printf("col = %d\n", A[0].size());
+
+
+    return 0;   
 }
